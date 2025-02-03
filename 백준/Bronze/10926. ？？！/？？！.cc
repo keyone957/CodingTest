bb7389f1fb60295ad6d69d22b90d707e79035a3e
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-02-03 19:53:24 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <functional>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string input;
+	cin >> input;
+	cout << input << "??!";
+
+}
