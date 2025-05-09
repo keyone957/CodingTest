e9fc9a4961aec5921631f17fb3a1e3c1f14a561e
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-05-09 14:18:13 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+int dp[1001][3];
+int R[1001];
+int G[1001];
+int B[1001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int count;
+	cin >> count;
+	for (int i = 1; i <= count; i++)
+	{
+
+		int r, g, b;
+		cin >> r >> g >> b;
+		R[i] = r;
+		G[i] = g;
+		B[i] = b;
+
+	}
+	dp[1][0] = R[1];
+	dp[1][1] = G[1];
+	dp[1][2] = B[1];
+	for (int i = 2; i <= count; i++)
+	{
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + R[i];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + G[i];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + B[i];
+	}
+	cout << *min_element(dp[count], dp[count] + 3);
+}
