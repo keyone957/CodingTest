affabe5c0bfa93930e3aec44889dd6fa032a3fcf
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-05-17 21:43:30 +0900

    [Gold V] Title: 퇴사 2, Time: 220 ms, Memory: 37056 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+int n;
+//long long d[1500001];
+//long long t[1500001];
+//long long p[1500001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long d[1500001];
+	long long t[1500001];
+	long long p[1500001];
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> t[i] >> p[i];
+	}
+	for (int i = n; i > 0; i--)
+	{
+		if (i + t[i] > n + 1)
+		{
+			d[i] = d[i + 1];
+		}
+		else
+		{
+			d[i] = max(d[i + 1], p[i] + d[i + t[i]]);
+		}
+	}
+	cout << d[1];
+}
