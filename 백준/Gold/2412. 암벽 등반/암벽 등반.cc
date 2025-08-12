785495fb55ca960d570f3f20967b4a2165bf7e9c
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-08-12 11:39:08 +0900

    [Gold IV] Title: 암벽 등반, Time: 32 ms, Memory: 2612 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.cc
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.cc
@@ -30,77 +30,65 @@ int main()
     {
         int x, y;
         cin >> x >> y;
-        board[i] = { x,y };
+        board[i] = { y,x };
     }
-    //sort(board.begin(), board.end());
-    sort(board.begin(), board.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-        if (a.second == b.second) return a.first < b.first;
-        return a.second < b.second;
-        });
+    sort(board.begin(), board.end());
+   
     queue<int> q; 
     for (int i = 0; i < n; i++)
     {
-        int bx = board[i].first;
-        int by = board[i].second;
+        int by = board[i].first;   // y좌표
+        int bx = board[i].second;  // x좌표
         if (abs(bx - 0) <= 2 && abs(by - 0) <= 2)
         {
             dist[i] = 1;
             q.push(i);
         }
     }
-    while (!q.empty()) 
+
+    while (!q.empty())
     {
         int cur = q.front();
         q.pop();
 
-        int x = board[cur].first;
-        int y = board[cur].second;
+        int y = board[cur].first;   // y좌표
+        int x = board[cur].second;  // x좌표
         int d = dist[cur];
 
-        if (y >= t) { // 목표 도달
+        if (y >= t) // 목표 도달
+        {
             cout << d;
             return 0;
         }
 
-        //for (int i = 0; i < n; i++)
-        //{
-        //    if (dist[i] != -1) continue; // 이미 방문
-        //    int nx = board[i].first;
-        //    int ny = board[i].second;
-
-        //    if (abs(x - nx) <= 2 && abs(y - ny) <= 2)
-        //    {
-        //        dist[i] = d + 1;
-        //        q.push(i);
-        //    }
-        //}
-         // 위쪽 탐색
-        for (int i = cur + 1; i < n; i++) {
-            int ny = board[i].second;
-            if (ny - y > 2) break; // 더 이상 못 감
+        // 위쪽 탐색
+        for (int i = cur + 1; i < n; i++)
+        {
+            int ny = board[i].first;
+            if (ny - y > 2) break; // y차이가 2 초과하면 중단
             if (dist[i] != -1) continue;
-            int nx = board[i].first;
-            if (abs(x - nx) <= 2) {
+            int nx = board[i].second;
+            if (abs(x - nx) <= 2)
+            {
                 dist[i] = d + 1;
                 q.push(i);
             }
         }
 
         // 아래쪽 탐색
-        for (int i = cur - 1; i >= 0; i--) {
-            int ny = board[i].second;
+        for (int i = cur - 1; i >= 0; i--)
+        {
+            int ny = board[i].first;
             if (y - ny > 2) break;
             if (dist[i] != -1) continue;
-            int nx = board[i].first;
-            if (abs(x - nx) <= 2) {
+            int nx = board[i].second;
+            if (abs(x - nx) <= 2)
+            {
                 dist[i] = d + 1;
                 q.push(i);
             }
         }
     }
-
     cout << -1;
     return 0;
-  
-
 }
