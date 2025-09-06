ce668708c70a7c7a894edfeb99e08ed7e5c171bc
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-09-06 11:19:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 20 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -11,69 +11,59 @@
 #include <stack>
 #include <string>
 #include <vector>
-int board[302][302];
-int dist[302][302];
-int isVisit[302][302];
+#include <unordered_set>
+#include<unordered_map>
 using namespace std;
+int dist[301][301];
+void clear()
+{
+    for (int i = 0; i < 301; i++)
+    {
+        for (int j = 0; j < 301; j++)
+        {
+            dist[i][j] = -1;
+        }
+    }
+}
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int dx[8] = { -1,1,2,2,1,-1,-2,-2 };
-	int dy[8] = { -2,-2,-1,1,2,2,1,-1 };
-	int input;
-	cin >> input;
-	for (int i = 0; i < input; i++)
-	{
-		int chessSize;
-		pair<int, int> startPoint;
-		pair<int, int> endPoint;
-		queue<pair<int, int>>Q;
-		cin >> chessSize;
-		cin >> startPoint.first >> startPoint.second;
-		cin >> endPoint.first >> endPoint.second;
-		for (int i = 0; i < 302; i++)
-		{
-			for (int j = 0; j < 302; j++)
-			{
-				board[i][j] = 0;
-				isVisit[i][j] = 0;
-				dist[i][j] = 0;
-			}
-		}
-		while (!Q.empty())
-		{
-			Q.pop();
-		}
-		for (int i = 0; i < chessSize; i++)
-		{
-			for (int j = 0; j < chessSize; j++)
-			{
-				dist[i][j] = -1;
-			}
-		}
-		dist[startPoint.first][startPoint.second] = 0;
-		Q.push(startPoint);
-		while (!Q.empty())
-		{
-			pair<int, int> cur = Q.front();
-			if (cur.first == endPoint.first && cur.second == endPoint.second)
-			{
-				cout << dist[endPoint.first][endPoint.second]  << "\n";
-				break;
-			}
-			Q.pop();
-			for (int i = 0; i < 8; i++)
-			{
-				int nx = cur.first + dx[i];
-				int ny = cur.second + dy[i];
-				if (nx < 0 || nx >= chessSize || ny < 0 || ny >= chessSize)continue;
-				if (dist[nx][ny] != -1)continue;
-				dist[nx][ny] = dist[cur.first][cur.second] + 1;
-				Q.push({ nx, ny });
-			}
-		}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int dx[8] = { -1,1,2,2,1,-1,-2,-2 };
+    int dy[8] = { -2,-2,-1,1,2,2,1,-1 };
+    int count;
+    cin >> count;
+    while (count--)
+    {
+        clear();
+        int boardSize, startX,startY,endX,endY;
+        cin >> boardSize;
+        cin >> startX >> startY;
+        cin >> endX >> endY;
+        queue<pair<int, int>> q;
+        q.push({ startX,startY });
+        dist[startX][startY] = 0;
+        while (!q.empty())
+        {
+            pair<int, int> cur = q.front(); q.pop();
+            if (cur.first == endX && cur.second == endY)
+            {
+                cout << dist[endX][endY] << '\n';
+                break;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                int nx = cur.first + dx[i];
+                int ny = cur.second + dy[i];
+                if (nx < 0 || nx >= boardSize || ny < 0 || ny >= boardSize)continue;
+                if (dist[nx][ny] != -1) continue;
+                dist[nx][ny] = dist[cur.first][cur.second] + 1;
+                q.push({ nx, ny });
+            }
 
-	}
+        }
+        
+    }
+
 }
