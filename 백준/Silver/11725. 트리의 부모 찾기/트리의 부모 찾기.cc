a507366036853781d56a014fcc852be48edeede9
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2025-11-25 17:57:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -11,50 +11,49 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
 using namespace std;
-vector<vector<int>> arr;
-vector<int> visit;
-vector<int> answer;
-void DFS(int node)
+vector<int> arr[100001];
+vector<int> parent;
+int n;
+void bfs(int cur)
 {
-	if (visit[node]==1)
-	{
-		return;
-	}
-	visit[node] = 1;
-	for (int i = 0; i < arr[node].size(); i++)
-	{
-		int next = arr[node][i];
-		// --> 시작점 부터 다음 노드 확인하면서 끝에 다시 시작점으로 돌아오면 
-		//visit된거이므로 이제 dfs멈춤
-		//자식 노드로 가기 직전에 현재 노드가 다음번에 탐색할 노드의 부모 노드임.
-		if (visit[next] != 1)
-		{
-			answer[next] = node;
-			DFS(next);
-		}
-	}
+    queue<int> q;
+    q.push(cur);
+    //parent[cur] = 0;
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+       
+        for (int i = 0; i < arr[cur].size(); i++)
+        {
+            int next = arr[cur][i];
+            if (parent[cur] == next) continue;
+            q.push(next);
+            parent[next] = cur;
+        }
+    }
 }
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int count;
-	cin >> count;
-	arr.resize(count + 1);
-	visit.resize(count + 1);
-	answer.resize(count + 1);
-	for (int i = 1; i < count; i++)
-	{
-		int start, end;
-		cin >> start >> end;
-		arr[start].push_back(end);
-		arr[end].push_back(start);
-	}
-	DFS(1);
-	for (int i = 2; i < answer.size(); i++)
-	{
-		cout << answer[i] << "\n";
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    parent.resize(n + 1);
+    for (int i = 0; i < n-1; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        arr[start].push_back(end);
+        arr[end].push_back(start);
+    }
+    bfs(1);
+    for (int i = 2; i <= n; i++)
+    {
+        cout << parent[i]<<'\n';
+    }
 }
