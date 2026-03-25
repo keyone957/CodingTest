890bb8a0986594ff65b08abffda82d7b510ce721
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2026-03-25 17:58:45 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -11,27 +11,31 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+#include<cstring>
 using namespace std;
-int dp[1000000];
+int x;
+int dp[1000001];
+//i에 도달 했을때 최소 연산 횟수
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	dp[1] = 0;
-	int n;
-	cin >> n;
-	for (int i = 2; i <= n; i++)
-	{
-		dp[i] = dp[i - 1] + 1;
-		if (i % 3 == 0)
-		{
-			dp[i] = min(dp[i], dp[i / 3] + 1);
-		}
-		if (i % 2 == 0)
-		{
-			dp[i] = min(dp[i], dp[i / 2] + 1);
-		}
-	}
-	cout << dp[n];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> x;
+    for (int i = 2; i <= x; i++)
+    {
+        dp[i]=dp[i-1]+ 1;
+        if (i % 3 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+    }
+    cout << dp[x];
 }
