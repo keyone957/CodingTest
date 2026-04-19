1e546ef7a3a377d424ad2eb7eaadd6b99c74c7b1
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2026-04-19 17:40:27 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.cc
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -11,44 +11,38 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+#include<cstring>
 using namespace std;
+string check;
+string findString;
+int answer;
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	string input;
-	getline(cin, input);
-	string checkWord;
-	getline(cin, checkWord);
-	int count = 0;
-	int wordSize = checkWord.size();
-	//while (input.find(checkWord) != string::npos)
-	//{
-	//	//cout << input.find(checkWord) << "찾음";
-	//	int index = input.find(checkWord);
-	//	input = input.erase(index, wordSize);
-	//	count++;
-	//}
-	/*
-		위에 원래 input을 삭제 하면 안되는게 예외가 있다.
-		aabb
-		ab
-		이러면 맨처음에 ab를 삭제를 하면
-		ab가 또 남아버림
-		그래서 답이 2로 나와버리는데 이건 오답임
-		따라서 원래 문자열을 건드리지말고 index만 옮겨보도록함
-	*/
-	size_t pos = 0;
-	string temp = input;
-	while (true) {
-		pos = temp.find(checkWord);
-		if (pos == string::npos) break;
-
-		count++;
-		// 찾은 위치 다음부터 새로운 부분 문자열 생성
-		temp = temp.substr(pos + wordSize);
-	}
-	cout << count;
-
+ 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    getline(cin, check);
+    getline(cin, findString);
+    int idx= 0;
+    while (idx <= (int)check.size() - (int)findString.size())
+    {
+        /*if (check[idx] == findString[0])
+        {
+           check.substr(idx,findString.size())
+        }*/
+        if (check.substr(idx, findString.size()) == findString)
+        {
+            answer++;
+            idx += findString.size();
+        }
+        else
+        {
+            idx++;
+        }
+    }
+    cout << answer;
 }
