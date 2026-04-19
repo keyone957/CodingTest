684db4c86b4bbdbb2aceb92d7ec3bd26d13f64a4
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2026-04-19 16:11:52 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -1,15 +1,44 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
 #include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+#include<cstring>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    int change = 1000 - n;
-    int coins[] = {500, 100, 50, 10, 5, 1};
-    int cnt = 0;
-    for (int c : coins) {
-        cnt += change / c;
-        change %= c;
+int m,answer;
+int rest[6] = { 500,100,50,10,5,1 };
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> m;
+    int restM = 1000 - m;
+    int i=0;
+    while (restM)
+    {
+        int check = restM - rest[i];
+        if (check >= 0)
+        {
+            restM = check;
+            answer++;
+        }
+        else
+        {
+            i++;
+        }
     }
-    cout << cnt;
+    cout << answer;
+
 }
